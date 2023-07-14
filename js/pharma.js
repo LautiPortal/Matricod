e0cfff238b411603dc40ce8851b5af4f85cf1e01
lu0tero <luotero.99@gmail.com>
2023-07-14 17:17:58 +0100

    maquetado de pharma

--- js/pharma.js
+++ js/pharma.js
@@ -1,24 +1,33 @@
-import image from '../assets/images/sections/pharma/'
 
 const pharmaData = [
     {
         title: 'Tipo de matriz 1',
-        image: `../assets/images/sections/pharma/alimentador1.jpg`,
+        image: '../assets/images/sections/pharma/alimentador1.jpg',
+        info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
+    },
+    {
+        title: 'Tipo de matriz 2',
+        image: '../assets/images/sections/pharma/alimentador2.jpg',
+        info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
+    },
+    {
+        title: 'Tipo de matriz 3',
+        image: '../assets/images/sections/pharma/alimentador3.jpg',
+        info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
+    },
+    {
+        title: 'Tipo de matriz 4',
+        image: '../assets/images/sections/pharma/alimentador4.jpg',
+        info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
+    },
+    {
+        title: 'Tipo de matriz 2',
+        image: '../assets/images/sections/pharma/alimentador4.jpg',
         info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
     },
     {
         title: 'Tipo de matriz 1',
-        image: '',
-        info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
-    },
-    {
-        title: 'Tipo de matriz 1',
-        image: '',
-        info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
-    },
-    {
-        title: 'Tipo de matriz 1',
-        image: '',
+        image: '../assets/images/sections/pharma/alimentador4.jpg',
         info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu ',
     },
 ]
@@ -28,10 +37,10 @@ const pharmaCardsContainer = document.getElementById('pharma-cards-container');
 const pharmaMap = pharmaData.map(product => {
     return (
         `<div class='pharma-card'>
-            <img src=${product.image}/>
+            <img class='pharma-card-img' src=${product.image} />
             <div class='pharma-card-info'>
-                <h3>${product.title}</h3>
-                <p>${product.info}</p>
+                <h3 class='pharma-card-title'>${product.title}</h3>
+                <p class='pharma-card-p'>${product.info}</p>
             </div>
         </div>`
     )
